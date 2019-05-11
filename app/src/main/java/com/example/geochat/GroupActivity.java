37ecae728336f2f8c12c9accafcb7cc5e63a08d0
Ieltxu <ieltxuteje@gmail.com>
2019-05-11 17:29:54 +0200

    Toolbar añadido

--- app/src/main/java/com/example/geochat/GroupActivity.java
+++ app/src/main/java/com/example/geochat/GroupActivity.java
@@ -4,24 +4,28 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
-
+import android.support.v7.widget.Toolbar;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
 public class GroupActivity extends AppCompatActivity {
     private ArrayList<Grupo> grupos;
+    String nick;
     Database database = new Database("https://134.209.235.115/gabad002/WEB/group.php", this, null);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group);
-
+        Toolbar bar= findViewById(R.id.bar);
+        setSupportActionBar(bar);
         Intent intent = getIntent();
-        String nick = intent.getStringExtra("nick");
+        nick = intent.getStringExtra("nick");
 
         AsyncTask<String, String, ArrayList<Grupo>> a = database.execute(nick);
 
@@ -47,5 +51,29 @@ public class GroupActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.valuesbar,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id=item.getItemId();
+        if(id==R.id.add_group){
+
+        }else if(id==R.id.signout){
+            Intent intent=new Intent(getApplicationContext(),LoginActivity.class);
+            startActivity(intent);
+        }else if(id==R.id.share_nick){
+            Intent sendIntent = new Intent();
+            sendIntent.setAction(Intent.ACTION_SEND);
+            sendIntent.putExtra(Intent.EXTRA_TEXT, "Este es mi nick: "+nick);
+            sendIntent.setType("text/plain");
+            startActivity(sendIntent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
